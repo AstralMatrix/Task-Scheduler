4256cc79defb30260702fdc5aaea3bd4aae626c4
Thomas Westpfal <westpfal385@gmail.com>
2021-05-17 14:45:43 -0400

    display tasks on run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from package.src.file_processor import FileProcessor
+from package.src.task import Task
 
 x = FileProcessor.load_csv("data/input.csv")
-for item in x:
-    print(item.display_str())
+for item in Task.display_tasks(x):
+    print(item)
