e3e8ec81c3d36b0f4ea0f659f683a42311d75450
AstralMatrix <westpfal385@gmail.com>
2021-05-25 04:10:16 -0400

    spawn gui window instead of printing tasks

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+import sys
 from package.src.file_processor import FileProcessor
 from package.src.task import Task
+from package.src.form import Form
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtGui import QIcon
 
-x = FileProcessor.load_csv("data/input.csv")
+'''x = FileProcessor.load_csv("data/input.csv")
 for item in Task.display_tasks(x):
     print(item)
+    '''
+
+app = QApplication(sys.argv)
+ex = Form()
+sys.exit(app.exec_())
