60e64de3d48cde481860c3726c421faa497fa9dc
AstralMatrix <westpfal385@gmail.com>
2021-05-25 05:05:08 -0400

    added menu button placeholder functions, and added typing info

--- package/src/form.py
+++ package/src/form.py
@@ -1,87 +1,117 @@
+from typing import List, Callable
 from package.src.file_processor import FileProcessor
 from package.src.task import Task
 from package.src.settings import Settings
 from package.src.error import Error
 from PyQt5.QtWidgets import QWidget, QMainWindow, QAction, QListWidget, \
-    QGridLayout, QFileDialog, QMessageBox
+    QGridLayout, QFileDialog, QMessageBox, QDesktopWidget, QMenuBar, QMenu
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QRect, QPoint
 
 
 class Form(QMainWindow):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.title = "TaskMaster"
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.title: str = "TaskMaster"
+        self.left: int = 10
+        self.top: int = 10
+        self.width: int = 640
+        self.height: int = 480
         self.initUI()
 
-    def initUI(self):
+    def initUI(self) -> None:
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        screen = QWidget()
+        # Move window to center of the screen
+        geom: QRect = self.frameGeometry()
+        center: QPoint = QDesktopWidget().availableGeometry().center()
+        geom.moveCenter(center)
+        self.move(geom.topLeft())
+
+        screen: QWidget = QWidget()
         self.setCentralWidget(screen)
 
-        layout = QGridLayout()
+        layout: QGridLayout = QGridLayout()
         screen.setLayout(layout)
 
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('File')
-        editMenu = mainMenu.addMenu('Edit')
+        mainMenu: QMenuBar = self.menuBar()
+        fileMenu: QMenu = mainMenu.addMenu('File')
+        editMenu: QMenu = mainMenu.addMenu('Edit')
 
-        openButton = QAction('Open File', self)
+        openButton: QAction = QAction('Open File', self)
         openButton.triggered.connect(self.openFile)
         fileMenu.addAction(openButton)
-        saveButton = QAction('Save File', self)
+        saveButton: QAction = QAction('Save File', self)
         saveButton.triggered.connect(self.saveFile)
         fileMenu.addAction(saveButton)
-        exitButton = QAction('Exit', self)
+        exitButton: QAction = QAction('Exit', self)
         exitButton.triggered.connect(self.close)
         fileMenu.addAction(exitButton)
 
-        addButton = QAction('Add Task', self)
+        addButton: QAction = QAction('Add Task', self)
+        addButton.triggered.connect(self.addTask)
         editMenu.addAction(addButton)
-        toggleButton = QAction('Toggle Task', self)
+        toggleButton: QAction = QAction('Toggle Task', self)
+        toggleButton.triggered.connect(self.toggleTask)
         editMenu.addAction(toggleButton)
-        removeButton = QAction('Remove Task', self)
+        removeButton: QAction = QAction('Remove Task', self)
+        removeButton.triggered.connect(self.removeTask)
         editMenu.addAction(removeButton)
 
-        self.taskList = self.createTaskList(
+        self.taskList: QListWidget = self.createTaskList(
             Task.display_tasks(
-                self.run(FileProcessor.load_csv, Settings.active_file, fb=[])))
+                self.run(
+                    FileProcessor.load_csv, Settings.active_file, fb=[])))
 
         layout.addWidget(self.taskList)
 
         self.show()
 
-    def createTaskList(self, data):
+    def createTaskList(self, data: List[str]) -> QListWidget:
         tl = QListWidget()
         tl.setFont(QFont('noto mono', 10))
         for item in data:
             tl.insertItem(0, item)
         return tl
 
-    def openFile(self):
-        options = QFileDialog.Options()
+    def openFile(self) -> None:
+        options: QFileDialog = QFileDialog.Options()
         fileName, _ = QFileDialog.getOpenFileName(
             self, "Open File", "Test", "CSV Files (*.csv);;All Files (*)",
             options=options)
-        tasks = self.run(FileProcessor.load_csv, fileName, fb=[])
+        tasks: List[Task] = self.run(FileProcessor.load_csv, fileName, fb=[])
         Settings.active_file = fileName
         print(fileName)
 
-        layout = self.taskList.parent().layout()
-        newTaskList = self.createTaskList(Task.display_tasks(tasks))
+        layout: QGridLayout = self.taskList.parent().layout()
+        newTaskList: QListWidget = self.createTaskList(
+            Task.display_tasks(tasks))
         layout.replaceWidget(self.taskList, newTaskList)
         self.taskList = newTaskList
 
-    def saveFile(self):
-        pass
+    def saveFile(self) -> None:
+        QMessageBox.information(
+            self, "info", f"Saving has not yet been "
+            f"added\nPlease edit the csv file itself\n{Settings.active_file}")
 
-    def run(self, function, *args, fb):
+    def addTask(self) -> None:
+        QMessageBox.information(
+            self, "info", f"Adding has not yet been "
+            f"added\nPlease edit the csv file itself\n{Settings.active_file}")
+
+    def toggleTask(self) -> None:
+        QMessageBox.information(
+            self, "info", f"Toggling has not yet been "
+            f"added\nPlease edit the csv file itself\n{Settings.active_file}")
+
+    def removeTask(self) -> None:
+        QMessageBox.information(
+            self, "info", f"Removing has not yet been "
+            f"added\nPlease edit the csv file itself\n{Settings.active_file}")
+
+    def run(self, function: Callable, *args, fb: any) -> any:
         try:
             return function(*args)
         except Error as e:
