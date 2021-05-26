2763ab52886ffa4af5951de839ad4fff2ab9ff8e
AstralMatrix <westpfal385@gmail.com>
2021-05-26 00:16:52 -0400

    raise error if wrong file encoding

--- package/src/file_processor.py
+++ package/src/file_processor.py
@@ -9,15 +9,18 @@ class FileProcessor:
     @staticmethod
     def load_csv(file_path: str) -> List[Task]:
         try:
-            with open(file_path, encoding='utf-8', errors='strict') as f:
+            with open(file_path, 'r', encoding='utf-8', errors='strict') as f:
                 data: Iterator = csv.reader(f)
-                return FileProcessor.extract_csv_data(data)
+                try:
+                    return FileProcessor.extract_csv_data(data)
+                except UnicodeDecodeError:
+                    raise Error(f"The file \"{file_path}\" can not be read, "
+                                f"please ensure it is a UTF-8 encoded file")
         except FileNotFoundError:
             raise Error(f"The file \"{file_path}\" does not exist")
 
     @staticmethod
     def extract_csv_data(csv_reader: Iterator) -> List[Task]:
-        next(csv_reader)  # Remove csv header.
         return Task.minimize_lateness(
             [Task.create_task(i, x) for i, x in enumerate(csv_reader)]
         )
