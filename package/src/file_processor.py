154779b9379c17b17295418cb24c04c4d4667ed2
AstralMatrix <westpfal385@gmail.com>
2021-06-13 14:42:02 -0400

    added csv saving, and updated error messages

--- package/src/file_processor.py
+++ package/src/file_processor.py
@@ -14,13 +14,23 @@ class FileProcessor:
                 try:
                     return FileProcessor.extract_csv_data(data)
                 except UnicodeDecodeError:
-                    raise Error(f"The file \"{file_path}\" can not be read, "
-                                f"please ensure it is a UTF-8 encoded file")
+                    raise Error(f"Cannot load, the file \"{file_path}\" can "
+                                f"not be read, please ensure it is a UTF-8 "
+                                f"encoded file")
         except FileNotFoundError:
-            raise Error(f"The file \"{file_path}\" does not exist")
+            raise Error(f"Cannot load, the file \"{file_path}\" does not exist")
 
     @staticmethod
     def extract_csv_data(csv_reader: Iterator) -> List[Task]:
         return Task.minimize_lateness(
             [Task.create_task(i, x) for i, x in enumerate(csv_reader)]
         )
+
+    @staticmethod
+    def save_csv(file_path: str, data: List[str]) -> None:
+        try:
+            with open(file_path, 'w', encoding='utf-8') as f:
+                for d in data:
+                    f.write(d + '\n')
+        except FileNotFoundError:
+            raise Error(f"Cannot save, the file \"{file_path}\" does not exist")
